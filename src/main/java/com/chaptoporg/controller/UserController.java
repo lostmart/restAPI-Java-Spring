22f7c8e307f136f2e0d57fabbe8fbb512cf0a193
lostmart <martinpedraza1979@gmail.com>
2025-06-10 22:42:43 +0200

    new methods

--- src/main/java/com/chaptoporg/controller/UserController.java
+++ src/main/java/com/chaptoporg/controller/UserController.java
@@ -1,18 +1,33 @@
 package com.chaptoporg.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
 @RestController
-@RequestMapping("/api/users")
 
 public class UserController {
-    @GetMapping("/")
-    public String welcome(){
-        return "🍀 Welcome to the Welcome to the ChâTop REST API!" ;
+    @GetMapping("/users")
+    public String welcome() {
+        return "🍀 Welcome to users endpoint !!";
     }
+
+    @GetMapping("/users/{id}")
+    public String welcomeById(@PathVariable String id) {
+        return "🍀 Welcome to users endpoint !! " + id;
+    }
+    
+    @PostMapping("/users")
+    public String postMethodName(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
 }
 
