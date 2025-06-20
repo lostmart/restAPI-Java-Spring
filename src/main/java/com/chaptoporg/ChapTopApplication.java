a0397d1fd4a317fd223388ab463fc64e988b46ea
lostmart <martinpedraza1979@gmail.com>
2025-06-20 20:04:18 +0200

    new env files

--- src/main/java/com/chaptoporg/ChapTopApplication.java
+++ src/main/java/com/chaptoporg/ChapTopApplication.java
@@ -3,10 +3,15 @@ package com.chaptoporg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ChapTopApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(ChapTopApplication.class, args);
     }
 
